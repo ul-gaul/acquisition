964df35e6e1c455008ed3f0a88e102c1d7ab8042
Chiminakust <maxime_glmt@hotmail.ca>
2018-06-03 15:54:32 -0400

    ajout libraries du bmp180

--- MainAcquisition/MainAcquisition/main.cpp
+++ MainAcquisition/MainAcquisition/main.cpp
@@ -7,6 +7,9 @@
 
 #include <mbed.h>
 #include "GPS.h"
+#include "BMP180.h"
+#include "LSM303DLHC.h"
+#include "L3GD20.h"
 #include "rocket_packet.h"
 
 // relay command related defines
@@ -25,6 +28,11 @@
 
 // GPS related defines
 #define GPS_SAMPLE_PERIOD 1	// 1Hz
+
+// 10DOF related defines
+#define IMU10DOF_SAMPLE_PERIOD 1	// 1Hz
+#define IMU10DOF_SDA PB_9
+#define IMU10DOF_SCL PB_8
 
 // relay GPIOs
 DigitalOut r_set_stm32(PD_6);
@@ -46,10 +54,22 @@ Serial ser_relays(PC_10, PC_11);
 // GPS object
 GPS gps(PD_8, PD_9);
 
+// I2C object for the communication with the 10DOF IMU breakout
+// This I2C bus will communicate with the L3DG20H (gyroscope),
+// LSM303DLHC (accelerometer) and the BMP180 (altitude and temperature)
+I2C i2cbus(IMU10DOF_SDA, IMU10DOF_SCL);
+
+// create 10DOF related sensors objects
+BMP180 bmp180(&i2cbus);
+LSM303DLHC lsm303(IMU10DOF_SDA, IMU10DOF_SCL);
+L3GD20 l3gd20(IMU10DOF_SDA, IMU10DOF_SCL);
+int ground_pressure;
+
 
 // Ticker (timer) object to send data to ground station
 Ticker rf_ticker;
 Ticker gps_ticker;
+Ticker imu190dof_ticker;
 
 // enum for the relay command decoding
 enum RelayDecodeStates {
@@ -66,6 +86,7 @@ RocketPacket rocket_packet;
 char* rocket_packet_serialized;
 unsigned int sizeof_rocket_packet;
 
+// public functions
 void ser_relay_handler(void);
 void decode_relay_command(uint8_t* cmd, uint8_t b);
 void execute_relay_command(uint8_t* cmd);
@@ -73,17 +94,33 @@ void serialize_rocket_packet(RocketPacket pkt, char* s);
 void send_packet();
 void update_gps();
 void update_10dof();
+float get_temperature();
+int get_pressure();
+float get_altitude(int groundpressure);
+
 
 int main() {
+	// attach various interrupts to timers and peripherals
 	ser_relays.attach(&ser_relay_handler);
 	rf_ticker.attach(&send_packet, RF_SEND_PERIOD);
 	gps_ticker.attach(&update_gps, GPS_SAMPLE_PERIOD);
-	// TODO attach 10DOF
+	imu190dof_ticker.attach(&update_10dof, IMU10DOF_SAMPLE_PERIOD);
+	DigitalOut led0(PD_15);
+	// initialize sensors
+	if(bmp180.init() != 0) {
+		// error with bmp180
+		while(1);
+	}
+	ground_pressure = get_pressure();
 	sizeof_rocket_packet = sizeof(unsigned long)
-								+ 4 * sizeof(float)
+								+ 13 * sizeof(float)
 								+ sizeof(uint8_t);
 	rocket_packet_serialized = (char *) malloc((size_t) sizeof_rocket_packet);
 	for (;;) {
+		//		ser_relays.putc(0x45);
+		//		wait_ms(10);
+		wait_ms(200);
+		led0 = !led0;
 	}
 }
 
@@ -131,6 +168,7 @@ void decode_relay_command(uint8_t* cmd, uint8_t b) {
 }
 
 void execute_relay_command(uint8_t* cmd) {
+	int s;
 	switch(cmd[0]) {
 	case GET_RELAY_STATE:
 		switch(cmd[1]) {
@@ -188,14 +226,36 @@ void serialize_rocket_packet(RocketPacket pkt, char* s) {
 	size_t offset = 0;
 	// copy struct into char array, using memcpy to avoid packing related bugs,
 	// sending everything in order
+	// timestamp
 	memcpy(s + offset, (void *) &pkt.data.timestamp, sizeof(pkt.data.timestamp));
 	offset += sizeof(pkt.data.timestamp);
-	memcpy(s + offset, (void *) &pkt.data.altitude, sizeof(pkt.data.altitude));
-	offset += sizeof(pkt.data.altitude);
+	// GPS values
 	memcpy(s + offset, (void *) &pkt.data.latitude, sizeof(pkt.data.latitude));
 	offset += sizeof(pkt.data.latitude);
 	memcpy(s + offset, (void *) &pkt.data.longitude, sizeof(pkt.data.longitude));
 	offset += sizeof(pkt.data.longitude);
+	memcpy(s + offset, (void *) &pkt.data.altitude, sizeof(pkt.data.altitude));
+	offset += sizeof(pkt.data.altitude);
+	memcpy(s + offset, (void *) &pkt.data.temperature, sizeof(pkt.data.temperature));
+	offset += sizeof(pkt.data.temperature);
+	memcpy(s + offset, (void *) &pkt.data.x_accel, sizeof(pkt.data.x_accel));
+	offset += sizeof(pkt.data.x_accel);
+	memcpy(s + offset, (void *) &pkt.data.y_accel, sizeof(pkt.data.y_accel));
+	offset += sizeof(pkt.data.y_accel);
+	memcpy(s + offset, (void *) &pkt.data.z_accel, sizeof(pkt.data.z_accel));
+	offset += sizeof(pkt.data.z_accel);
+	memcpy(s + offset, (void *) &pkt.data.x_magnet, sizeof(pkt.data.x_magnet));
+	offset += sizeof(pkt.data.x_magnet);
+	memcpy(s + offset, (void *) &pkt.data.y_magnet, sizeof(pkt.data.y_magnet));
+	offset += sizeof(pkt.data.y_magnet);
+	memcpy(s + offset, (void *) &pkt.data.z_magnet, sizeof(pkt.data.z_magnet));
+	offset += sizeof(pkt.data.z_magnet);
+	memcpy(s + offset, (void *) &pkt.data.x_gyro, sizeof(pkt.data.x_gyro));
+	offset += sizeof(pkt.data.x_gyro);
+	memcpy(s + offset, (void *) &pkt.data.y_gyro, sizeof(pkt.data.y_gyro));
+	offset += sizeof(pkt.data.y_gyro);
+	memcpy(s + offset, (void *) &pkt.data.z_gyro, sizeof(pkt.data.z_gyro));
+	offset += sizeof(pkt.data.z_gyro);
 	// compute checksum
 	pkt.checksum = 0;
 	for(int i = 0 ; i < offset ; i++) {
@@ -220,5 +280,38 @@ void update_gps() {
 }
 
 void update_10dof() {
-	// TODO read 10DOF values and update rocket data
+	// update accelerometer and magnetometer values
+	lsm303.read(&rocket_data.x_accel, &rocket_data.y_accel, &rocket_data.z_accel,
+				&rocket_data.x_magnet, &rocket_data.y_magnet, &rocket_data.z_magnet);
+	// update temperature and altitude values
+	rocket_data.temperature = get_temperature();
+	rocket_data.altitude = get_altitude(ground_pressure);
+	// update gyroscope values
+	l3gd20.read(&rocket_data.x_gyro, &rocket_data.y_gyro, &rocket_data.z_gyro);
+}
+
+float get_temperature() {
+	float temperature;
+	bmp180.startTemperature();
+	wait_ms(5);	// wait for conversion to complete
+	if(bmp180.getTemperature(&temperature) != 0) {
+		return -1.0;
+	}
+	return temperature;
+}
+
+int get_pressure() {
+	int pressure;
+	bmp180.startPressure(BMP180::ULTRA_LOW_POWER);
+	wait_ms(10);
+	if(bmp180.getPressure(&pressure) != 0) {
+		return -1;
+	}
+	return pressure;
+}
+
+float get_altitude(int groundpressure) {
+	int pressure;
+	pressure = get_pressure();
+	return 44330 * (1 - pow(pressure / groundpressure, 1.0 / 5.255));
 }
--- MainAcquisition/MainAcquisition/rocket_packet.h
+++ MainAcquisition/MainAcquisition/rocket_packet.h
@@ -11,9 +11,21 @@
 
 typedef struct {
 	unsigned long timestamp;
-	float altitude;
+	// GPS values
 	float latitude;
 	float longitude;
+	// 10DOF values
+	float altitude;
+	float temperature;
+	float x_accel;
+	float y_accel;
+	float z_accel;
+	float x_magnet;
+	float y_magnet;
+	float z_magnet;
+	float x_gyro;
+	float y_gyro;
+	float z_gyro;
 } RocketData;
 
 typedef struct {
