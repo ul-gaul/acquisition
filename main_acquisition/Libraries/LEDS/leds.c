9bdcfd0b000ca4e4f5dddf0026bbd893dde29921
Maxime Guillemette <maxime_glmt@hotmail.ca>
2018-09-16 15:06:04 -0400

    added init led function

--- main_acquisition/Libraries/LEDS/leds.c
+++ main_acquisition/Libraries/LEDS/leds.c
@@ -6,3 +6,17 @@
  */
 
 #include "leds.h"
+
+void init_leds() {
+	// enable the clock on the GPIO port
+	RCC_AHB1PeriphClockCmd(LED_PORT, ENABLE);
+	// configure GPIOs as output
+	GPIO_InitTypeDef GPIO_InitOut;
+	GPIO_InitOut.GPIO_Pin = LED1 | LED2 | LED3 | LED4 | LED5 | LED6;
+	GPIO_InitOut.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitOut.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitOut.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitOut.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(LED_TYPEDEF_PORT, &GPIO_InitOut);
+}
+
--- main_acquisition/Libraries/LEDS/leds.h
+++ main_acquisition/Libraries/LEDS/leds.h
@@ -8,6 +8,23 @@
 #ifndef LEDS_LEDS_H_
 #define LEDS_LEDS_H_
 
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
 
+// LED port and number defines
+#define LED_PORT RCC_AHB1Periph_GPIOE
+#define LED_TYPEDEF_PORT GPIOE
+#define LED1 GPIO_Pin_10
+#define LED2 GPIO_Pin_11
+#define LED3 GPIO_Pin_12
+#define LED4 GPIO_Pin_13
+#define LED5 GPIO_Pin_14
+#define LED6 GPIO_Pin_15
+
+/*
+ * Brief:
+ * 		Initialize the LEDs on the acquisition board.
+ */
+void init_leds();
 
 #endif /* LEDS_LEDS_H_ */
