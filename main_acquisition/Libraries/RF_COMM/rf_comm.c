83d059efd36031341e326ef39f45d451b17cb4a8
Maxime Guillemette <maxime_glmt@hotmail.ca>
2018-09-16 16:20:11 -0400

    added init function for USART3 periph

--- main_acquisition/Libraries/RF_COMM/rf_comm.c
+++ main_acquisition/Libraries/RF_COMM/rf_comm.c
@@ -6,3 +6,40 @@
  */
 
 #include "rf_comm.h"
+
+void init_rfd900() {
+	// enable clock and alternative function on GPIOs
+	GPIO_InitTypeDef gpio_init_struct;
+	RCC_AHB1PeriphClockCmd(RFD_PORT, ENABLE);
+	GPIO_PinAFConfig(RFD_TYPEDEF_PORT, RFD_TX, GPIO_AF_USART3);
+	GPIO_PinAFConfig(RFD_TYPEDEF_PORT, RFD_RX, GPIO_AF_USART3);
+	gpio_init_struct.GPIO_Pin = RFD_RX | RFD_TX;
+	gpio_init_struct.GPIO_Mode = GPIO_Mode_AF;
+	gpio_init_struct.GPIO_OType = GPIO_OType_PP;
+	gpio_init_struct.GPIO_PuPd = GPIO_PuPd_UP;
+	gpio_init_struct.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_Init(gpio_typedef, &gpio_init_struct);
+	// initialize USART peripheral
+	USART_InitTypeDef USART_InitStruct;
+	// enable clock for USART peripheral
+	RCC_APB2PeriphClockCmd(RFD_USART_PERIPH, ENABLE);
+	// initialize and activate USART peripheral
+	USART_InitStruct.USART_BaudRate = RFD_BAUDRATE;
+	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+	USART_InitStruct.USART_Parity = USART_Parity_No;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	USART_Init(RFD_USART_PERIPH_TYPEDEF, &USART_InitStruct);
+	USART_Cmd(RFD_USART_PERIPH_TYPEDEF, ENABLE);
+	// NOTE: uncomment this section if RX is used and interrupts are required
+	// NVIC_InitTypeDef NVIC_InitStruct;
+	// enable RX interrupt
+	// USART_ITConfig(RFD_USART_PERIPH_TYPEDEF, USART_IT_RXNE, ENABLE);
+	// initialize NVIC for RX interrupts
+	// NVIC_InitStruct.NVIC_IRQChannel = USART3_IRQn;
+	// NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+	// NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+	// NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+	// NVIC_Init(&NVIC_InitStruct);
+}
--- main_acquisition/Libraries/RF_COMM/rf_comm.h
+++ main_acquisition/Libraries/RF_COMM/rf_comm.h
@@ -8,6 +8,17 @@
 #ifndef RF_COMM_RF_COMM_H_
 #define RF_COMM_RF_COMM_H_
 
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
 
+#define RFD_PORT RCC_AHB1Periph_GPIOD
+#define RFD_TYPEDEF_PORT GPIOD
+#define RFD_USART_PERIPH RCC_APB2Periph_USART3
+#define RFD_USART_PERIPH_TYPEDEF USART3
+#define RFD_TX GPIO_Pin_8
+#define RFD_RX GPIO_Pin_9
+#define RFD_BAUDRATE 9600
+
+void init_rfd900();
 
 #endif /* RF_COMM_RF_COMM_H_ */
