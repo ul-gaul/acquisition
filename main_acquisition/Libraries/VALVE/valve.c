137ae4f5a687dc8abf8e1aff4aec37a53dab6793
Maxime Guillemette <maxime_glmt@hotmail.ca>
2018-11-01 10:54:30 -0400

    ajout fichiers pour drivers valve de la bonbonne du moteur

---
+++ main_acquisition/Libraries/VALVE/valve.c
@@ -0,0 +1,9 @@
+/*
+ * valve.c
+ *
+ *  Created on: Nov 1, 2018
+ *      Author: laplace
+ */
+
+
+#include "valve.h"
---
+++ main_acquisition/Libraries/VALVE/valve.h
@@ -0,0 +1,16 @@
+/*
+ * valve.h
+ *
+ *  Created on: Nov 1, 2018
+ *      Author: laplace
+ *
+ *  Brief:
+ *  	Controls the motor's tank valve
+ */
+
+#ifndef VALVE_VALVE_H_
+#define VALVE_VALVE_H_
+
+
+
+#endif /* VALVE_VALVE_H_ */
