affddbb4adb0e97f730a613eb29e2e81437ce0c2
Maxime Guillemette <maxime_glmt@hotmail.ca>
2018-11-01 20:13:57 -0400

    ajout lib valve

--- main_acquisition/Libraries/VALVE/valve.c
+++ main_acquisition/Libraries/VALVE/valve.c
@@ -5,5 +5,31 @@
  *      Author: laplace
  */
 
-
 #include "valve.h"
+
+void init_valve() {
+	// enable the clock on the GPIO port
+	RCC_AHB1PeriphClockCmd(VALVE_PORT, ENABLE);
+	// configure GPIOs as output
+	GPIO_InitTypeDef GPIO_InitOut;
+	GPIO_InitOut.GPIO_Pin = VALVE_GPIO;
+	GPIO_InitOut.GPIO_Mode = GPIO_Mode_OUT;
+	GPIO_InitOut.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitOut.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_InitOut.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(VALVE_TYPEDEF_PORT, &GPIO_InitOut);
+}
+
+void set_valve_on() {
+	GPIO_SetBits(VALVE_TYPEDEF_PORT, VALVE_GPIO);
+}
+
+void set_valve_off() {
+	GPIO_ResetBits(VALVE_TYPEDEF_PORT, VALVE_GPIO);
+}
+
+uint8_t get_valve_state() {
+	uint16_t a;
+	a = VALVE_TYPEDEF_PORT->ODR;
+	return 0;
+}
--- main_acquisition/Libraries/VALVE/valve.h
+++ main_acquisition/Libraries/VALVE/valve.h
@@ -11,6 +11,36 @@
 #ifndef VALVE_VALVE_H_
 #define VALVE_VALVE_H_
 
+#include "stm32f4xx.h"
+#include "stm32f4_discovery.h"
 
+#define VALVE_PORT RCC_AHB1Periph_GPIOE
+#define VALVE_TYPEDEF_PORT GPIOE
+#define VALVE_GPIO GPIO_Pin_9
+
+/*
+ * Brief:
+ * 		Initialize the valve GPIO on the acquisition
+ * 		board.
+ */
+void init_valve();
+
+/*
+ * Brief:
+ * 		Sets the valve's enable signal to high.
+ */
+void set_valve_on();
+
+/*
+ * Brief:
+ * 		Sets the valve's enable signal to low.
+ */
+void set_valve_off();
+
+/*
+ * Brief:
+ * 		Reads the valve's state
+ */
+uint8_t get_valve_state();
 
 #endif /* VALVE_VALVE_H_ */
