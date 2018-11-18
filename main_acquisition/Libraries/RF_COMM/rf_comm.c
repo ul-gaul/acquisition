f244202d489dc66c6ecf2c2674cd7e58693e7346
Maxime Guillemette <maxime_glmt@hotmail.ca>
2018-11-18 16:46:59 -0500

    transmission TX transmet le premier char de la string test

--- main_acquisition/Libraries/RF_COMM/rf_comm.c
+++ main_acquisition/Libraries/RF_COMM/rf_comm.c
@@ -8,41 +8,49 @@
 #include "rf_comm.h"
 
 void init_rfd900() {
-	// enable clock and alternative function on GPIOs
+	// enable clock for USART3 and GPIOD
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+
+	// configure GPIOs
 	GPIO_InitTypeDef gpio_init_struct;
-	RCC_AHB1PeriphClockCmd(RFD_PORT, ENABLE);
-	GPIO_PinAFConfig(RFD_TYPEDEF_PORT, RFD_TX, GPIO_AF_USART3);
-	GPIO_PinAFConfig(RFD_TYPEDEF_PORT, RFD_RX, GPIO_AF_USART3);
-	gpio_init_struct.GPIO_Pin = RFD_RX | RFD_TX;
+	gpio_init_struct.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
 	gpio_init_struct.GPIO_Mode = GPIO_Mode_AF;
 	gpio_init_struct.GPIO_OType = GPIO_OType_PP;
 	gpio_init_struct.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	gpio_init_struct.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(RFD_TYPEDEF_PORT, &gpio_init_struct);
+
+	// connect USART pins to their alternate functions
+	GPIO_PinAFConfig(RFD_TYPEDEF_PORT, GPIO_PinSource8, GPIO_AF_USART3);
+	GPIO_PinAFConfig(RFD_TYPEDEF_PORT, GPIO_PinSource9, GPIO_AF_USART3);
+
 	// initialize USART peripheral
 	USART_InitTypeDef USART_InitStruct;
-	// enable clock for USART peripheral
-	RCC_APB2PeriphClockCmd(RFD_USART_PERIPH, ENABLE);
-	// initialize and activate USART peripheral
 	USART_InitStruct.USART_BaudRate = RFD_BAUDRATE;
+	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+	USART_InitStruct.USART_StopBits = USART_StopBits_1;
+	USART_InitStruct.USART_Parity = USART_Parity_No;
 	USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
 	USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-	USART_InitStruct.USART_Parity = USART_Parity_No;
-	USART_InitStruct.USART_StopBits = USART_StopBits_1;
-	USART_InitStruct.USART_WordLength = USART_WordLength_8b;
-	USART_Init(RFD_USART_PERIPH_TYPEDEF, &USART_InitStruct);
-#if RX_ENABLE
-	NVIC_InitTypeDef NVIC_InitStruct;
-	// enable RX interrupt
+	USART_Init(USART3, &USART_InitStruct);
+
+	// enable USART
+	USART_Cmd(USART3, ENABLE);
+
+	// setup interrupts
 	USART_ITConfig(RFD_USART_PERIPH_TYPEDEF, USART_IT_RXNE, ENABLE);
+
+#if RX_ENABLE
 	// initialize NVIC for RX interrupts
+	NVIC_InitTypeDef NVIC_InitStruct;
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
 	NVIC_InitStruct.NVIC_IRQChannel = USART3_IRQn;
-	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
-	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 5;
+	NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
 	NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStruct);
 #endif /* RX_ENABLE */
-	USART_Cmd(RFD_USART_PERIPH_TYPEDEF, ENABLE);
 }
 
 void rfd900_write(uint8_t* src, uint16_t size) {
