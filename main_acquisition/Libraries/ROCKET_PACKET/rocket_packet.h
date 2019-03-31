52bb7059d30fbb4489cc9f0141064199a3757427
Maxime Guillemette <maxime_glmt@hotmail.ca>
2019-03-31 15:51:35 -0400

    added units to rocket packet

--- main_acquisition/Libraries/ROCKET_PACKET/rocket_packet.h
+++ main_acquisition/Libraries/ROCKET_PACKET/rocket_packet.h
@@ -18,6 +18,7 @@
 
 typedef struct {
 
+	// time since boot in milliseconds
 	double timestamp; // 8
 	// GPS values
 	double latitude; // 16
@@ -27,20 +28,24 @@ typedef struct {
 	double UTCTime; // 48
 	// 10DOF values
 	// BMP180
+	
 	float altitude; // 52
 	uint32_t pressure; // 56
 	float temperature; // 60
 	// lsm303
+	// acceleration values are in milli-G
 	uint16_t acc_x_uncomp; // 62
 	uint16_t acc_y_uncomp; // 64
 	uint16_t acc_z_uncomp; // 66
 	float acc_x; // 70
 	float acc_y; // 74
 	float acc_z; // 78
+	// magnetic field values are in milli-gauss
 	uint16_t mag_x; // 80
 	uint16_t mag_y; // 82
 	uint16_t mag_z; // 84
 	// l3dg20
+	// angular speed values are in degrees/s
 	int16_t x_gyro; // 86
 	int16_t y_gyro; // 88
 	int16_t z_gyro; // 90
